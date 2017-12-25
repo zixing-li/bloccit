a62857e1e5ac57aa3b5cb72c7538771c5f56f1ec
Luke Li <zl82@cornell.edu>
2017-12-25 08:44:10 +0000

    checkpoint_14_assignment

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+   let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+ 
+ # #2
+   describe "attributes" do
+     it "has title and body attributes" do
+       expect(question).to have_attributes(title: "New Question Title", body: "New Question Body")
+     end
+   end
+end
